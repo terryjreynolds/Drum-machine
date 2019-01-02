a5f90be4156fda6482ca1973f2b178488d39be34
Terry Reynolds <terrancejreynolds@gmail.com>
2019-01-02 14:09:57 -0500

    touch action manipulation added to css

--- script.js
+++ script.js
@@ -30,11 +30,9 @@ buttons.map(c => setUpEventListener(c));
 
 function setUpEventListener(listItem) {
   console.log(document.querySelector(`#${listItem}`));
-  document
-    .querySelector(`#${listItem}`)
-    .addEventListener("mousedown", function() {
-      playAudio(listItem);
-    });
+  document.querySelector(`#${listItem}`).addEventListener("click", function() {
+    playAudio(listItem);
+  });
 }
 
 //listen for key presses
